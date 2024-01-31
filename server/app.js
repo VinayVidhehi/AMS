36d8c3b8a2714c1b9942bc5eb4d39d7e7caa6083
VinayVidhehi <vinayvidhehi@gmail.com>
2024-01-31 13:37:17 +0530

    updated everything as discussed due wednesday

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { handleUserLogin, handleUserSignup, attendanceUpdate, getAttendance, getStaffAttendance, updateCourseDetails } = require('./utils/router');
+
+const app = express();
+const PORT = 7800;
+
+// Enable CORS
+app.use(cors());
+
+// Parse incoming request bodies in a middleware before your handlers
+app.use(bodyParser.json());
+
+// MySQL Connection
+
+// Change the route to /attendance
+app.get('/attendance', attendanceUpdate);
+app.get('/get-attendance', getAttendance);
+app.get('/attendance-staffview', getStaffAttendance)
+
+app.post('/signup', handleUserSignup);
+app.post('/login', handleUserLogin);
+app.post('/update-course', updateCourseDetails);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
